d1526e07c0a00c9bdd4799aa16fe1808f7d66a22
Ledze <47141308+Ledze@users.noreply.github.com>
2024-07-30 14:30:58 +0200

    Add files via upload

---
+++ Training.MessageDelivery.Common/Models/MessagesOnHistoryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.MessageDelivery.Common.Models
+{
+    public class MessagesOnHistoryModel
+    {
+        public List<MessagesOnTopic> Messages { get; set; }   
+    }
+
+    public class MessagesOnTopic
+    {
+        public string MessageId { get; set; }
+        public string MessageText { get; set; }
+        public DateTime EnqueuedTime { get; set; }
+    }
+}
---
+++ Training.MessageDelivery.Common/Models/SubscriptionsForTopicModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.MessageDelivery.Common.Models
+{
+    public class SubscriptionsForTopicModel
+    {
+        public List<string> Subscriptions { get; set; }
+    }
+}
